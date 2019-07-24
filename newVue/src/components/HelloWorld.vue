b116afae4fb0ce0d2986e0bfcf2cd65ca15bb863
liyl <370306875@qq.com>
2019-07-24 16:52:09 +0800

    添加sync修饰 绑定父子组件

--- newVue/src/components/HelloWorld.vue
+++ newVue/src/components/HelloWorld.vue
@@ -5,9 +5,12 @@
     <div>{{this.$store.state.num}}</div>
     <div>{{this.$store.state.data}}</div>
     <button @click="mountion()">改变姓氏</button>
-    <children :name='msg' @childByValue="parent"></children>
+    <children :name="msg" @childByValue="parent => Bus = parent" :foo.sync="fatherFoo"></children>
     <p>{{childValue}}</p>
     <p>{{Bus}}</p>
+    <!-- sync修饰使得父子组件绑定 -->
+    <p>{{fatherFoo}}</p>
+    <button @click="FatherUpdate()">父组件来点击改变</button>
   </div>
 </template>
 
@@ -15,18 +18,19 @@
 import { mapMutations } from "vuex";
 import http from "../http/http.js";
 import children from "@/components/children";
-import bus from '../assets/bus';
+import bus from "../assets/bus";
 
 export default {
   name: "HelloWorld",
-  components:{
-     children:children,
+  components: {
+    children: children
   },
   data() {
     return {
-      msg:'小明',
-      childValue:'',
-      Bus:''
+      msg: "小明",
+      childValue: "",
+      Bus: "",
+      fatherFoo: ""
     };
   },
   compute: {
@@ -34,20 +38,17 @@ export default {
   },
   mounted() {
     mapMutations.callCheng;
-    console.log(this.$store.state.name);
-    this.$axios.post('/post',{name:'李'})
-      .then(res=>{
-        console.log(res)
-      })
-    http.test()
-    .then(res=>{
-      console.log(res)
-    })
-    
-    bus.$on('bus',res=>{
-      console.log(res)
-      this.Bus = res
-    })
+    this.$axios.post("/post", { name: "李" }).then(res => {
+      console.log(res);
+    });
+    http.test().then(res => {
+      console.log(res);
+    });
+
+    bus.$on("bus", res => {
+      console.log(res);
+      this.Bus = res;
+    });
   },
   methods: {
     mountion() {
@@ -55,8 +56,8 @@ export default {
       this.$store.dispatch("callAdd", 20);
       this.$store.getters.callCheng;
     },
-    parent(res){
-      this.childValue = res;
+    FatherUpdate() {
+      this.fatherFoo = "被父组件改变";
     }
   }
 };
