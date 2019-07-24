9e7a88293d8c677623d2e7ba0f12babc2b46eb1b
liyl <370306875@qq.com>
2019-07-24 16:51:17 +0800

    添加cync修饰 使得父子组件绑定

--- newVue/src/components/children.vue
+++ newVue/src/components/children.vue
@@ -1,38 +1,40 @@
 <!-- 组件说明 -->
 <template>
-    <div class='content'>
-        <h1>子组件</h1>
-        <p>{{name}}</p>
-        <button @click="send">子传父</button>
-        <button @click="Bus()">兄弟传值</button>
-    </div>
+  <div class="content">
+    <h1>子组件</h1>
+    <p>{{name}}</p>
+    <p>{{foo}}</p>
+    <button @click="send">子传父</button>
+    <button @click="Bus()">兄弟传值</button><br>
+    <button @click="updateFoo">点击改变</button>
+  </div>
 </template>
 
 <script>
-    import bus from '../assets/bus';
-    export default {
-        name:'children',
-        data () {
-            return {
-                clild:'child',
-                bus:'兄弟',
-            };
-        },
-        computed: {
-
-        },
-        methods: {
-            send(){
-                this.$emit('childByValue', this.clild)
-            },
-            Bus(){
-                bus.$emit('bus',this.bus)
-            }
-        },
-         props:['name']
+import bus from "../assets/bus";
+export default {
+  name: "children",
+  data() {
+    return {
+      clild: "child",
+      bus: "兄弟"
+    };
+  },
+  computed: {},
+  methods: {
+    send() {
+      this.$emit("childByValue", this.clild);
+    },
+    Bus() {
+      bus.$emit("bus", this.bus);
+    },
+    updateFoo() {
+      this.$emit("update:foo", "foo被子组件改变啦");
     }
+  },
+  props: ["name", "foo"]
+};
 </script>
 
 <style scoped>
-
 </style>
